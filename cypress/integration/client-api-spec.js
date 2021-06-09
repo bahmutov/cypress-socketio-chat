396fef9423a1c3b63754c0a744613b9d0ddb8874
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-09 13:08:38 -0400

    refactor

--- cypress/integration/client-api-spec.js
+++ cypress/integration/client-api-spec.js
@@ -1,21 +1,24 @@
 /// <reference types="cypress" />
 
-it.skip('chats', () => {
+it('shows status for 2nd user', () => {
   const name = `Cy_${Cypress._.random(1000)}`
+  cy.log(`User **${name}**`)
   cy.visit('/', {
     onBeforeLoad(win) {
       cy.stub(win, 'prompt').returns(name)
     },
   })
+
   cy.get('#txt').type('Hello there{enter}')
-  cy.contains('#messages li', 'Hello there').contains('strong', name)
+  cy.contains('#messages li', 'Hello there')
+    .contains('strong', name)
+    .then(() => {
+      cy.log('**second user**')
+    })
 
   // pretend to send a message from another user
-  cy.window()
-    .its('clientActions')
-    .invoke('isOnline', '👻 <i>Ghost is testing</i>')
-  cy.window()
-    .its('clientActions')
-    .invoke('onChatMessage', '<strong>Ghost</strong>: Boo')
+  cy.window().its('clientActions').as('client')
+  cy.get('@client').invoke('isOnline', '👻 <i>Ghost is testing</i>')
+  cy.get('@client').invoke('onChatMessage', '<strong>Ghost</strong>: Boo')
   cy.contains('#messages li', 'Boo').contains('strong', 'Ghost')
 })
