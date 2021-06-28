564ae8930c92e48d458798e65f5e92243cf1c254
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-06-28 10:32:51 -0400

    show how to use mock socket connection

---
+++ cypress/integration/mock-socket-spec.js
@@ -0,0 +1,77 @@
+/// <reference types="cypress" />
+
+import SocketMock from 'socket.io-mock'
+
+describe('Mock socket', () => {
+  // these tests "trick" the application by injecting
+  // a mock socket from the test into the application
+  // instead of letting the application connect to the real one
+  const socket = new SocketMock()
+
+  // store info about the client connected from the page
+  let username
+  let lastMessage
+  socket.socketClient.on('username', (name) => {
+    console.log('user %s connected', name)
+    username = name
+    // broadcast to everyone, mimicking the index.js server
+    socket.socketClient.emit(
+      'is_online',
+      '🔵 <i>' + username + ' join the chat..</i>',
+    )
+  })
+
+  socket.socketClient.on('chat_message', (message) => {
+    console.log('user %s says "%s"', username, message)
+    lastMessage = '<strong>' + username + '</strong>: ' + message
+    socket.socketClient.emit('chat_message', lastMessage)
+  })
+
+  it('chats', () => {
+    cy.intercept('/', (req) => {
+      req.continue((res) => {
+        res.body = res.body.replace(
+          "io.connect('http://localhost:8080')",
+          'window.testSocket',
+        )
+      })
+    }).as('html')
+
+    // the browser is the 1st user
+    const name = `Cy_${Cypress._.random(1000)}`
+
+    cy.log(`User **${name}**`)
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        win.testSocket = socket
+        cy.stub(win, 'prompt').returns(name)
+      },
+    })
+    cy.wait('@html') // our HTML intercept has worked
+      // verify we have received the username
+      // use .should(callback) to retry
+      // until the variable username has been set
+      .should(() => {
+        expect(username, 'username').to.equal(name)
+      })
+
+    // try sending a message
+    cy.get('#txt').type('Hello there{enter}')
+    cy.contains('#messages li', 'Hello there').contains('strong', name)
+
+    // verify the mock socket has received the message
+    cy.should(() => {
+      expect(lastMessage, 'the right text').to.include('Hello there')
+      expect(lastMessage, 'the sender').to.include(name)
+    }).then(() => {
+      // emit message from the test socket
+      // to make sure the page shows it
+      socket.socketClient.emit(
+        'chat_message',
+        '<strong>Cy</strong>: Mock socket works!',
+      )
+
+      cy.contains('#messages li', 'Mock socket works').contains('strong', 'Cy')
+    })
+  })
+})
