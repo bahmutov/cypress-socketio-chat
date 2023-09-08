172e41f46a736772fc5e384781371b22dc49bd21
dharshqa <dharshqa@gmail.com>
2023-09-08 16:38:44 -0400

    addedTest1

--- cypress/pair/second-user.js
+++ cypress/pair/second-user.js
@@ -4,6 +4,4 @@
 it('interacts with Google', () => {
   cy.visit('http://www.google.ca');
   cy.url().should('eq', 'https://www.google.ca/?gws_rd=ssl'); // Check if URL matches.
-  cy.get('input[name="q"]').type('Cypress testing{enter}'); // Perform a search.
-  cy.get('#search').should('be.visible'); // Assert that search results are displayed.
 });
