fa4f2babb3e57cf6afb33773dbe97a197ce673d2
dharshqa <dharshqa@gmail.com>
2023-09-08 13:49:22 -0400

    scriptchanged

--- run-cypress-tests.sh
+++ run-cypress-tests.sh
@@ -4,10 +4,10 @@
 npm run chat:run -- --record --key $CYPRESS_RECORD_KEY
 
 # Capture the exit code of the Cypress tests
-$E2E_RESULT=$?
+E2E_RESULT=$?
 
 # Check if Cypress tests failed (non-zero exit code)
-if [ ["$E2E_RESULT" != "0"]]; then
+if [ "$E2E_RESULT" != "0" ]; then
     echo -e "\n$E2E_RESULT\n"
     exit 1
 fi
