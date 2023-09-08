0df08af473c320ddcc8df240bd5aed6fb22474f0
dharshqa <dharshqa@gmail.com>
2023-09-08 16:34:18 -0400

    testUpdate

--- cypress/pair/second-user.js
+++ cypress/pair/second-user.js
@@ -2,8 +2,8 @@
 
 // this test behaves as the second user to join the chat
 it('interacts with Google', () => {
-  cy.visit('http://www.google.com');
-  cy.url().should('eq', 'https://www.google.com/'); // Check if URL matches.
+  cy.visit('http://www.google.ca');
+  cy.url().should('eq', 'https://www.google.ca'); // Check if URL matches.
   cy.get('input[name="q"]').type('Cypress testing{enter}'); // Perform a search.
   cy.get('#search').should('be.visible'); // Assert that search results are displayed.
 });
