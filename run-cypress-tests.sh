20e4412fd6c6335569c00900ce39d4762e4d1aee
dharshqa <dharshqa@gmail.com>
2023-09-08 13:07:54 -0400

    recordaded

--- run-cypress-tests.sh
+++ run-cypress-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Cypress tests and record with parallel mode
-npm run chat:run --record --key $CYPRESS_RECORD_KEY --parallel
+npm run chat:run -- --record --key $CYPRESS_RECORD_KEY --parallel
 
 # Capture the exit code of the Cypress tests
 exit_code=$?
