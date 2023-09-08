87ca29eb28d28ad897081717493a44247f315d21
dharshqa <dharshqa@gmail.com>
2023-09-07 23:03:53 -0400

    changeconfigfile

--- cypress/pair/first-user.js
+++ cypress/pair/first-user.js
@@ -4,5 +4,10 @@
 it('chats with the second user', () => {
   const name = 'First'
   const secondName = 'Second'
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
   cy.visit('/');
 })
