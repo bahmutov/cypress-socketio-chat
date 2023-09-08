6b391962d5fced0696863b0968d89bf9bf0da483
dharshqa <dharshqa@gmail.com>
2023-09-08 12:08:38 -0400

    exitcodeadded

--- run-cypress-tests.sh
+++ run-cypress-tests.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
 
-
+# Run Cypress tests
 npm run chat:run -- --record --key $CYPRESS_RECORD_KEY
+
+# Capture the exit code of the Cypress tests
+exit_code=$?
+
+# Check if Cypress tests failed (non-zero exit code)
+if [ $exit_code -ne 0 ]; then
+    echo "Cypress tests failed with exit code $exit_code"
+    exit $exit_code
+fi
+
+# If the tests passed, exit with a success code (0)
+exit 0
