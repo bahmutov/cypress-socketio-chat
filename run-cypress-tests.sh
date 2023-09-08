93e98b8b22eb2e122e1aa1312073136cf9c101ee
dharshqa <dharshqa@gmail.com>
2023-09-08 13:56:02 -0400

    newexit

--- run-cypress-tests.sh
+++ run-cypress-tests.sh
@@ -7,7 +7,10 @@ npm run chat:run -- --record --key $CYPRESS_RECORD_KEY
 E2E_RESULT=$?
 
 # Check if Cypress tests failed (non-zero exit code)
-if [ "$E2E_RESULT" != "0" ]; then
-    echo -e "\n$E2E_RESULT\n"
-    exit 1
+if [ "$E2E_RESULT" -ne 0 ]; then
+    echo -e "\nCypress tests failed with exit code: $E2E_RESULT\n"
+    exit 1  # Exit the script with a status code of 1
+else
+    echo -e "\nCypress tests passed\n"
+    exit 0  # Exit the script with a status code of 0 (success)
 fi
