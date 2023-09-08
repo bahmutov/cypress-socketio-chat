70f95bd0a970f6fb6b4ccc302fb50977247c0f63
dharshqa <dharshqa@gmail.com>
2023-09-08 15:13:44 -0400

    old

--- cypress/pair/chat.js
+++ cypress/pair/chat.js
@@ -1,43 +1,28 @@
-const arg = require('arg');
+const arg = require('arg')
 
 const args = arg({
   '--open': Boolean,
   '--port': Number,
   '--record': Boolean,
   '--key': String,
-});
+  '--parallel': Boolean,
+})
 
 const port = args['--port'] || 9090;
 const record = args['--record'];
 const key = args['--key'];
+const parallel = args['--parallel'];
 
-const cypress = require('cypress');
-const io = require('socket.io')(port);
+const cypress = require('cypress')
+const io = require('socket.io')(port)
 
-// Create an array to store results
-const results = [];
-
-// Function to print test results
-// Function to print test results
-function printTestResults(result) {
-  if (result && result.spec && result.spec.name) {
-    console.log('Spec:', result.spec.name);
-    console.log('Tests:', result.totalTests);
-    console.log('Passing:', result.totalPassed);
-    console.log('Failing:', result.totalFailed);
-    console.log('Pending:', result.totalPending);
-    console.log('Skipped:', result.totalSkipped);
-    console.log('\n');
-  } else {
-    console.error('Invalid test results:', result);
-  }
-}
+let exitCode = 0;
 
 // little utility for delaying any async action
 const wait = (ms) => {
   return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    setTimeout(resolve, ms)
+  })
 }
 
 // Socket.io server to let two Cypress runners communicate and wait for "checkpoints"
@@ -46,80 +31,66 @@ const wait = (ms) => {
 // keep the last checkpoint around
 // even if a test runner joins later, it
 // should still receive it right away
-let lastCheckpoint;
+let lastCheckpoint
 
 io.on('connection', (socket) => {
-  console.log('chat new connection');
+  console.log('chat new connection')
   if (lastCheckpoint) {
-    console.log('sending the last checkpoint "%s"', lastCheckpoint);
-    socket.emit('checkpoint', lastCheckpoint);
+    console.log('sending the last checkpoint "%s"', lastCheckpoint)
+    socket.emit('checkpoint', lastCheckpoint)
   }
 
   socket.on('disconnect', () => {
-    console.log('disconnected');
-  });
+    console.log('disconnected')
+  })
 
   socket.on('checkpoint', (name) => {
-    console.log('chat checkpoint: "%s"', name);
-    lastCheckpoint = name;
-    io.emit('checkpoint', name);
-  });
-});
+    console.log('chat checkpoint: "%s"', name)
+    lastCheckpoint = name
+    io.emit('checkpoint', name)
+  })
+})
 
 // TODO: implement reset before each test
 
 if (args['--open']) {
-  console.log('opening the first Cypress');
+  console.log('opening the first Cypress')
 } else {
-  console.log('starting the first Cypress');
+  console.log('starting the first Cypress')
 }
 
-const cypressAction = args['--open'] ? cypress.open : cypress.run;
+const cypressAction = args['--open'] ? cypress.open : cypress.run
 const firstCypress = cypressAction({
   configFile: 'cy-first-user.config.js',
-  record,
-  key,
+  record,key,parallel,
+}).then((results) => {
+    if(results.totalFailed != 0)
+    {
+      exitCode = 1;
+      console.log(`Exit Code for First Cypress is: ${exitCode}`);
+      return results;
+    }   
 })
-.then((firstResults) => {
-  console.log('First Cypress has finished');
-  results.push(firstResults);
-  printTestResults(firstResults);
-})
-.catch((error) => {
-  console.error('Error running the first Cypress:', error);
-  process.exit(1);
-});
 
 // delay starting the second Cypress instance
 // to avoid XVFB race condition
 const secondCypress = wait(5000).then(() => {
-  console.log('starting the second Cypress');
+  console.log('starting the second Cypress')
   return cypressAction({
     configFile: 'cy-second-user.config.js',
-    record,
-    key,
+    record,key,parallel,
+  }).then((results) => {
+    if(results.totalFailed != 0)
+    {
+      exitCode = 1;
+      console.log(`Exit Code for Second Cypress is: ${exitCode}`);
+      return results;
+    }   
   });
 })
-.then((secondResults) => {
-  console.log('Second Cypress has finished');
-  results.push(secondResults);
-  printTestResults(secondResults);
-})
-.catch((error) => {
-  console.error('Error running the second Cypress:', error);
-  process.exit(1);
-});
 
-Promise.all([firstCypress, secondCypress])
-.then(() => {
+Promise.all([firstCypress, secondCypress]).then(() => {
   // TODO: exit with the test code from both runners
-  console.log('all done, exiting');
-  // Extract exit codes from results and exit with the highest one
-  const exitCodes = results.map((result) => result.totalFailed);
-  const highestExitCode = Math.max(...exitCodes);
-  process.exit(highestExitCode);
+  console.log(`Exit Code: ${exitCode}`);
+  exitCode == 1 ? process.exit(1) : process.exit(0);
 })
-.catch((error) => {
-  console.error('Error running Cypress:', error);
-  process.exit(1);
-});
