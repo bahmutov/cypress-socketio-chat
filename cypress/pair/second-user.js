acbf36d0587ef6543aba6a3a3fc7a714c57e201c
dharshqa <dharshqa@gmail.com>
2023-09-08 15:21:17 -0400

    removed test

--- cypress/pair/second-user.js
+++ cypress/pair/second-user.js
@@ -1,14 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Second user 2', () => {
-  // this test behaves as the second user to join the chat
-  it('chats with the first user', () => {
-    cy.visit('/');
-  })
-
-  it('Fail Assetion to test', () => {
-    //This Assetion will fail
-    cy.wrap(2 + 2).should('eq', 5);
-  })
+// this test behaves as the second user to join the chat
+it('chats with the first user', () => {
+  cy.visit('/');
 })
-
